778b6d5d17af0e1628c8d8d1b07135e57198c93a
Daniel <dandcalvo@gmail.com>
2024-12-03 12:09:39 -0800

    roadmap

--- src/challenges/2024/1/totalDistance.js
+++ src/challenges/2024/1/totalDistance.js
@@ -5,8 +5,13 @@ const list2 = [];
 
 const lines = input.split(/\n/);
 
+// extract data
 for (let line of lines) {
     const items = line.replace(/\r/, '').split('   ');
     list1.push(items[0]);
     list2.push(items[1]);
 }
+
+// sort left and right lists
+// calculate distance between each
+// sum total distance
