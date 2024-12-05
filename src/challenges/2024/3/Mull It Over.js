d9935d3e39902c82fdade4e7b0eeadcaaa2651bf
Daniel <dandcalvo@gmail.com>
2024-12-04 16:17:47 -0800

    third star

--- src/challenges/2024/3/Mull It Over.js
+++ src/challenges/2024/3/Mull It Over.js
@@ -1,2 +1,43 @@
-const mulRegex = /mul\(\d+,\d+\)/g;
+import input from './input.txt';
 
+console.log(solve(input));
+
+function solve() {
+    const instances = findInstances(input);
+    const params = extractParams(instances);
+    const total = mulAll(params);
+
+    return total;
+}
+
+function findInstances(input) {
+    return input.match(/mul\(\d+,\d+\)/g);
+}
+
+function extractParams(instances) {
+    const regex = /\d+,\d+/g;
+    const result = [];
+
+    let params = [];
+
+    for (let instance of instances) {
+        params.push(instance.match(regex));
+    }
+
+    for (let param of params) {
+        param = param[0].split(',');
+        param = param.map((elem) => +elem);
+        result.push(param);
+    }
+
+    return result;
+}
+
+function mulAll(params) {
+    return params.reduce(
+        (total, current) => total + current[0] * current[1],
+        0
+    );
+}
+
+/* module.exports = { findInstances, extractParams, mulAll }; */
--- src/challenges/2024/3/Mull It Over.test.js
+++ src/challenges/2024/3/Mull It Over.test.js
@@ -1,4 +1,32 @@
+const { findInstances, extractParams, mulAll } = require('./Mull It Over');
+
 const input =
     'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))';
 
-    
+describe('find instances of mul(X,X) and the total sum of all multiplications', () => {
+    const instances = findInstances(input);
+
+    test('return an array of each occurence', () => {
+        expect(instances).toEqual([
+            'mul(2,4)',
+            'mul(5,5)',
+            'mul(11,8)',
+            'mul(8,5)',
+        ]);
+    });
+
+    const parameters = extractParams(instances);
+
+    test('extract the mul parameters and store in a 2D Array', () => {
+        expect(parameters).toEqual([
+            [2, 4],
+            [5, 5],
+            [11, 8],
+            [8, 5],
+        ]);
+    });
+
+    test('calculate the product of each parameter pair then calculate the total sum', () => {
+        expect(mulAll(parameters)).toBe(161);
+    });
+});
